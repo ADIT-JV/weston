592e5165fab9f6595733fe716b25eb3f6bd78e1c
Hiroshi Noda <hnoda@jp.adit-jv.com>
2015-02-13 15:08:55 +0900

    build: Add build/include directroy and placed config.h
    Signed-off-by: Hiroshi Noda <hnoda@jp.adit-jv.com>

---
+++ build/include/config.h
@@ -0,0 +1,35 @@
+/**
+ * \file: config.h
+ *
+ * \version: $Id:$
+ *
+ * \release: $Name:$
+ *
+ * <brief description>.
+ * <detailed description>
+ * \component: <componentname>
+ *
+ * \author: <author>
+ *
+ * \copyright (c) 2012, 2013 Advanced Driver Information Technology.
+ * This code is developed by Advanced Driver Information Technology.
+ * Copyright of Advanced Driver Information Technology, Bosch, and DENSO.
+ * All rights reserved.
+ *
+ * \see <related items>
+ *
+ * \history
+ * <history item>
+ * <history item>
+ * <history item>
+ *
+ ***********************************************************************/
+#ifndef CONFIG_H
+#define CONFIG_H
+
+/*
+ * This header file is dummy for avoiding compile error
+ * compositor.h (/usr/include/weston) include config.h, but it doesn't exist.
+ */
+
+#endif
