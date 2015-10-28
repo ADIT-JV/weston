77dc89e000cd0162855324fdae01ead9b8a2a7e0
Emre Ucan <eucan@de.adit-jv.com>
2015-10-28 08:25:55 +0100

    gal2d-renderer: initial implementation
    Signed-off-by: Emre Ucan <eucan@de.adit-jv.com>

---
+++ src/gal2d-renderer.h
@@ -0,0 +1,57 @@
+/*
+ * Copyright (c) 2014 Freescale Semiconductor, Inc.
+ * Copyright © 2013 Vasily Khoruzhick <anarsoul@gmail.com>
+ *
+ * Permission to use, copy, modify, distribute, and sell this software and
+ * its documentation for any purpose is hereby granted without fee, provided
+ * that the above copyright notice appear in all copies and that both that
+ * copyright notice and this permission notice appear in supporting
+ * documentation, and that the name of the copyright holders not be used in
+ * advertising or publicity pertaining to distribution of the software
+ * without specific, written prior permission.  The copyright holders make
+ * no representations about the suitability of this software for any
+ * purpose.  It is provided "as is" without express or implied warranty.
+ *
+ * THE COPYRIGHT HOLDERS DISCLAIM ALL WARRANTIES WITH REGARD TO THIS
+ * SOFTWARE, INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS, IN NO EVENT SHALL THE COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * SPECIAL, INDIRECT OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER
+ * RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF
+ * CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN
+ * CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+#ifndef __gal_2d_renderer_h_
+#define __gal_2d_renderer_h_
+
+#include  "compositor.h"
+#ifdef ENABLE_EGL
+#include <EGL/egl.h>
+#else
+#include <HAL/gc_hal_eglplatform.h>
+typedef HALNativeDisplayType NativeDisplayType;
+typedef HALNativeWindowType  NativeWindowType;
+#endif
+
+
+struct gal2d_renderer_interface {
+
+	int (*create)(struct weston_compositor *ec,int use_drm);
+
+	int (*output_create)(struct weston_output *output,
+				NativeDisplayType display,
+				NativeWindowType window,
+				void* buffers,
+				unsigned n_buffers);
+
+	void (*output_destroy)(struct weston_output *output);
+};
+
+/* is shared between backend and renderer
+ * to provide native buffer information*/
+struct navite_drm_buf_info {
+	unsigned int w,h,stride;
+	/* Used by dumb fbs */
+	void *map;
+};
+
+#endif
