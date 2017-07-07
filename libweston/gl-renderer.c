d250918a3025fd9c962e4ddc4b1f9ecf92664b58
Emre Ucan <eucan@de.adit-jv.com>
2017-07-07 15:24:12 +0200

    gl-renderer: swapBuffers after filling vm_table
    Signed-off-by: Emre Ucan <eucan@de.adit-jv.com>

--- libweston/gl-renderer.c
+++ libweston/gl-renderer.c
@@ -1112,6 +1112,7 @@ gl_renderer_repaint_output(struct weston_output *output,
 	 */
 	if (output->enable_surface_share) {
 		vm_table_draw(output, go, gr);
+		ret = eglSwapBuffers(gr->egl_display, go->egl_surface);
 		return;
 	}
 
