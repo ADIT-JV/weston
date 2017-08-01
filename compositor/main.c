28c381cb077fb2e71103c38e88a7cb6ee62f878a
Emre Ucan <eucan@de.adit-jv.com>
2017-08-01 10:18:33 +0200

    main: parse enable-surface-share config
    It is used to activate surface-share for
    an output
    
    Signed-off-by: Emre Ucan <eucan@de.adit-jv.com>

--- compositor/main.c
+++ compositor/main.c
@@ -1165,6 +1165,7 @@ drm_backend_output_configure(struct wl_listener *listener, void *data)
 	char *modeline = NULL;
 	char *gbm_format = NULL;
 	char *seat = NULL;
+	int enable_surface_share;
 
 	if (!api) {
 		weston_log("Cannot use weston_drm_output_api.\n");
@@ -1185,6 +1186,11 @@ drm_backend_output_configure(struct wl_listener *listener, void *data)
 		s = NULL;
 	}
 	free(s);
+
+	weston_config_section_get_bool(section, "enable-surface-share",
+				       &enable_surface_share,
+				       0);
+	output->enable_surface_share = enable_surface_share;
 
 	if (api->set_mode(output, mode, modeline) < 0) {
 		weston_log("Cannot configure an output using weston_drm_output_api.\n");
