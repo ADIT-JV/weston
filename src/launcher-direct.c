ADIT-JV/weston

commit f17176bb9ecf9bf135457f66de0e648af16125e9

Run weston as system user instead of root
Signed-off-by: Michael Teyfel <mteyfel@de.adit-jv.com>

src/launcher-direct.c
@@ -272,20 +272,12 @@ launcher_direct_connect(struct weston_launcher **out, struct weston_compositor *
 {
 	struct launcher_direct *launcher;
 
-	if (geteuid() != 0)
-		return -EINVAL;
-
 	launcher = zalloc(sizeof(*launcher));
 	if (launcher == NULL)
 		return -ENOMEM;
 
 	launcher->base.iface = &launcher_direct_iface;
 	launcher->compositor = compositor;
-
-	if (setup_tty(launcher, tty) == -1) {
-		free(launcher);
-		return -1;
-	}
 
 	* (struct launcher_direct **) out = launcher;
 	return 0;

recipes/weston/files/weston.service
@@ -9,10 +9,10 @@ DefaultDependencies=no
 Type=notify
 Environment=XDG_RUNTIME_DIR=/tmp
 WatchdogSec=0
-ExecStart=/usr/bin/weston --tty=1 --modules=systemd-notify.so
+ExecStart=/usr/bin/weston --modules=systemd-notify.so
 Nice=-20
 UMask=0017
-User=root
+User=system
 # primary group graphics to create wayland socket with graphics user
 Group=graphics
 SupplementaryGroups=graphics drm input adit_errmem_log

commit a38c08292f0a0a7e61441e373e3a19ea9df2b20b

launcher: Fix seg-fault on weston exit
The previous commit:

f17176bb9ecf ("Run weston as system user instead of root")

causes a segmentation fault when weston exits:

[19:00:43.549] caught signal: 11
[19:00:43.549]   [000000000041e35a]  --  (weston)
[19:00:43.549]   [000000000041e3c7]  --  (weston)
[19:00:43.549]   [00000033eac330e0]  --  (/lib64/libc.so.6)
[19:00:43.549]   [00000033f2a092e5]  wl_event_source_remove  (/usr/lib64/libwayland-server.so.0)
[19:00:43.549]   [00007fa78deb2e56]  --  (/usr/lib64/weston/drm-backend.so)
[19:00:43.549]   [00007fa78deabf48]  --  (/usr/lib64/weston/drm-backend.so)
[19:00:43.549]   [0000000000412509]  weston_compositor_destroy  (weston)
[19:00:43.549]   [0000000000408d21]  --  (weston)
[19:00:43.549]   [00000033eac202c1]  __libc_start_main  (/lib64/libc.so.6)
[19:00:43.549]   [000000000040a42a]  _start  (weston)

Specifically this was caused by de-referencing launcher->vt_source which
was not set because setup_tty() was skipped. Fix this by calling setup_tty()
for root users as before, and then check for launcher->tty >=0 and
launcher->vt_source != NULL before their usage in case of non-root users.

It's not clear why the previous commit disabled setup_tty() for non-root
users, but likely the reason is that non-root users require the
CAP_SYS_TTY_CONFIG capability to configure tty's.

Fixes: f17176bb9ecf ("Run weston as system user instead of root")

Signed-off-by: Steve Longerbeam <steve_longerbeam@mentor.com>

src/launcher-direct.c
@@ -242,6 +242,9 @@ launcher_direct_restore(struct weston_launcher *launcher_base)
 	struct launcher_direct *launcher = wl_container_of(launcher_base, launcher, base);
 	struct vt_mode mode = { 0 };
 
+	if (launcher->tty < 0)
+		return;
+
 	if (ioctl(launcher->tty, KDSKBMUTE, 0) &&
 	    ioctl(launcher->tty, KDSKBMODE, launcher->kb_mode))
 		weston_log("failed to restore kb mode: %m\n");
@@ -263,6 +266,10 @@ static int
 launcher_direct_activate_vt(struct weston_launcher *launcher_base, int vt)
 {
 	struct launcher_direct *launcher = wl_container_of(launcher_base, launcher, base);
+
+	if (launcher->tty < 0)
+		return 0;
+
 	return ioctl(launcher->tty, VT_ACTIVATE, vt);
 }
 
@@ -279,6 +286,22 @@ launcher_direct_connect(struct weston_launcher **out, struct weston_compositor *
 	launcher->base.iface = &launcher_direct_iface;
 	launcher->compositor = compositor;
 
+	if (geteuid() == 0) {
+		if (setup_tty(launcher, tty) == -1) {
+			free(launcher);
+			return -1;
+		}
+	} else {
+		/*
+		 * FIXME: non-root users will need CAP_SYS_TTY_CONFIG
+		 * capability to configure the tty. For now, weston
+		 * will use whatever vt is currently active, in whatever
+		 * configuration it is currently in (hopefully graphics
+		 * mode?).
+		 */
+		launcher->tty = -1;
+	}
+
 	* (struct launcher_direct **) out = launcher;
 	return 0;
 }
@@ -289,7 +312,8 @@ launcher_direct_destroy(struct weston_launcher *launcher_base)
 	struct launcher_direct *launcher = wl_container_of(launcher_base, launcher, base);
 
 	launcher_direct_restore(&launcher->base);
-	wl_event_source_remove(launcher->vt_source);
+	if (launcher->vt_source)
+		wl_event_source_remove(launcher->vt_source);
 
 	if (launcher->tty >= 0)
 		close(launcher->tty);